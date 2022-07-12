e6030f70e07990965853b8e779057e93652baefc
Phil Owen <powen@renci.org>
2022-07-12 14:36:03 -0400

    Revert "Merge pull request #5 from RENCI/licensing"
    This reverts commit 05601aecedc5bda0d8b10ba5149c87ccc3d9f9ee, reversing
    changes made to 6d838ee595797f544a391c958d5b982ec2bf8d86.

--- files/setenv.sh
+++ files/setenv.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# SPDX-FileCopyrightText: 2022 Renaissance Computing Institute. All rights reserved.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-# SPDX-License-Identifier: LicenseRef-RENCI
-# SPDX-License-Identifier: MIT
-
 if [ -r "$CATALINA_HOME/bin/javaopts.sh" ]; then
   . "$CATALINA_HOME/bin/javaopts.sh"
 fi
